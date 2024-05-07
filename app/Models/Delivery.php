fe25e75640061513ac9a634ce109dba625320667
duds <duadsal@gmail.com>
2024-05-07 20:11:58 -0300

    Creating relationship in model delivery

--- app/Models/Delivery.php
+++ app/Models/Delivery.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Delivery extends Model
 {
@@ -17,4 +19,24 @@ class Delivery extends Model
         'tracking_id',
         'recipient_id'
     ];
+
+    public function carrier() : BelongsTo
+    {
+        return $this->belongsTo(Carrier::class, 'carrier_uuid', 'uuid');
+    }
+
+    public function sender() : BelongsTo
+    {
+        return $this->belongsTo(Sender::class, 'sender_id');
+    }
+
+    public function recipient() : BelongsTo
+    {
+        return $this->belongsTo(Recipient::class, 'recipient_id');
+    }
+
+    public function tracking() : HasOne
+    {
+        return $this->hasOne(Tracking::class, 'id', 'tracking_id');
+    }
 }
