ef0ac16d4f8929d77602c0c61016e8a160ecb7d4
duds <duadsal@gmail.com>
2024-05-12 19:36:00 -0300

    Service Integration

--- app/Http/Services/IntegrationService.php
+++ app/Http/Services/IntegrationService.php
@@ -3,43 +3,60 @@
 namespace App\Http\Services;
 
 use App\Facades\ApiDelivery;
+use App\Http\Services\Integration\IntegrationCarrierService;
+use App\Http\Services\Integration\IntegrationDeliveryService;
+use App\Http\Services\Integration\IntegrationRecepientService;
+use App\Http\Services\Integration\IntegrationSenderService;
+use App\Http\Services\Integration\IntegrationTrackingService;
+use App\Interfaces\IntegrationInterface;
+use App\Models\Carrier;
 use App\Models\Delivery;
 use App\Models\Recipient;
+use App\Models\Sender;
+use App\Models\Tracking;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
-class IntegrationService
+class IntegrationService implements IntegrationInterface
 {
 
-    public static function integrationDelivery($filteredData)
+    public function storeIntegrationCarrier(Collection $carrier_data): void
     {
-        foreach ($filteredData as $data) {
-            
-            $delivery_uuid =$data['_id'];
-            $volumes = $data['_volumes'];
-            $carrier_uuid = $data['_id_transportadora'];
-            
-            $sender_data = $data['_remetente'];
-            $recipient_data = $data['_destinatario'];
-            $tracking_data = $data['_rastreamento'];
-
-            $sender = SenderService::storeIntegrationSender($sender_data);
-            $tracking = TrackingService::storeIntegrationTracking($tracking_data);
-            $recipient = RecipientService::storeIntegrationRecipient($recipient_data);
-
-            $delivery = new Delivery();
-
-            $delivery->delivery_uuid = $delivery_uuid;
-            $delivery->volumes = $volumes;
-            $delivery->carrier_uuid = $carrier_uuid;
-            $delivery->sender_id = $sender['id'];
-            $delivery->tracking_id = $tracking['id'];
-            $delivery->recipient_id = $recipient['id'];
-
-            $delivery->save();
-    
-        }
-
+        $new_carrier = new IntegrationCarrierService();
+        $new_carrier->storeCarrier($carrier_data);
     }
 
+    public function storeIntegrationDelivery(Collection $delivery): void
+    {
+
+        $new_delivery = new IntegrationDeliveryService();
+        $new_delivery->storeDelivery($delivery);
+    }
+
+    public function storeIntegrationSender(Array $senderData): Sender
+    {
+        $new_sender = new IntegrationSenderService();
+        $sender = $new_sender->storeSender($senderData);
+
+        return $sender;
+    }
+
+    public function storeIntegrationRecepient(Array $recepientData): Recipient
+    {
+        $newRecepient = new IntegrationRecepientService();
+        $recepient = $newRecepient->storeRecepient($recepientData);
+
+        return $recepient;
+    }
+
+    public function storeIntegrationTracking(Array $trackingData, mixed $delivery_uuid) : Tracking
+    {
+        $newTracking = new IntegrationTrackingService();
+        $tracking = $newTracking->storeTracking($trackingData,$delivery_uuid);
+
+        return $tracking;
+    }
+    
 }
